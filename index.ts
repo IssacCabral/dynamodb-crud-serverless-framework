b6958cccc3b104e475428ea18b9284eb8894c8e8
Issac Cabral <issaccabrall@gmail.com>
2023-02-25 23:08:01 -0300

    feat: create delete and find All lambdas

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { DynamoDB } from "aws-sdk";
-import { CreateUserParams, InsertUser } from "./src/types/user.types";
+import { CreateUserParams } from "./src/types/user.types";
 import * as uuid from "uuid";
 
 class UserCrud {
@@ -30,6 +30,53 @@ class UserCrud {
       return {
         statusCode: 200,
         body: JSON.stringify(dbParams.Item),
+      };
+    } catch (error) {
+      console.error(error.stack);
+      return {
+        statusCode: 500,
+      };
+    }
+  }
+
+  async delete(event: APIGatewayEvent) {
+    try {
+      console.log(event.pathParameters!.id);
+
+      const params = {
+        TableName: this.dynamoDbTable,
+        Key: {
+          id: event.pathParameters!.id,
+          nameUser: "Romeo Cabral",
+        },
+      };
+
+      await this.dynamodDbService.delete(params).promise();
+
+      return {
+        statusCode: 200,
+        body: true,
+      };
+    } catch (error) {
+      console.error(error.stack);
+      return {
+        statusCode: 500,
+      };
+    }
+  }
+
+  async findAll(event: APIGatewayEvent) {
+    try {
+      const params = {
+        TableName: this.dynamoDbTable,
+      };
+
+      const result = await this.dynamodDbService.scan(params).promise();
+      const items = result.Items?.map((item) => item);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(items),
       };
     } catch (error) {
       console.error(error.stack);
--- src/factory.ts
+++ src/factory.ts
@@ -5,5 +5,7 @@ const dynamoDb = new DynamoDB.DocumentClient();
 const userCrud = new UserCrud(dynamoDb, process.env.DYNAMODB_TABLE ?? "");
 
 const create = userCrud.create.bind(userCrud);
+const deleteUser = userCrud.delete.bind(userCrud);
+const findAllUsers = userCrud.findAll.bind(userCrud);
 
-export { create };
+export { create, deleteUser, findAllUsers };
